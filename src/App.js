0a5b49c93aadba828e93f6c248da216fe52d2d39
Vivek Mehto <76930847+vivekmehto@users.noreply.github.com>
2021-06-26 12:40:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Music</h1>
+      <h3>Checkout my favourite music. Select genre to get started.</h3>
+    </div>
+  );
+}
